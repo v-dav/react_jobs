200702e3b43a70358bfbdef159e583c1f07b5524
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-05-10 12:32:43 +0200

    change proxy target to deployed mock api

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://test-api-ui2v.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
