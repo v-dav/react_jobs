bc4a190c588bb3c2e557f889d0a9457b8c1ee9d2
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-05-10 10:55:31 +0200

    fix settings for correct deployement

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react_jobs',
+  base: '/react_jobs/',
   plugins: [react()],
   server: {
     port: 3000,
