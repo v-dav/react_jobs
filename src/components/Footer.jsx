a22f15895f3b6219f3f0584e4010f4675efca7d5
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-05-10 13:09:13 +0200

    add footer to the app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+	  <div className="flex justify-center mb-5 mt-5 gap-2">Built by 
+		  <a className="text-indigo-500" href="https://github.com/v-dav"> Vladimir Davidov</a>
+	  </div>
+  )
+}
+
+export default Footer
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from '../components/Navbar'
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css'
+import Footer from '../components/Footer'
 
 const MainLayout = () => {
   return (
@@ -9,6 +10,7 @@ const MainLayout = () => {
 		  <Navbar />
 		  <Outlet />
 		  <ToastContainer />
+		  <Footer/>
 	  </>
   )
 }
