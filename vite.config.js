0cc7435ea63935445fbd6a7f70deffced38de790
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-05-10 11:22:22 +0200

    try to fix deployment bug

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react_jobs',
   server: {
     port: 3000,
     proxy: {
